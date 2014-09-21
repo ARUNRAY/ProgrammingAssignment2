2f42701f2d45ca70bfcc98c442aaa96e3b44ffa3
ARUNRAY <arun.rayroth@gmail.com>
2014-09-21 13:15:53 +1000

    Assignment ProgrammingAssignment2 for R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions which allow
+## for the storage of the inverse of a matrix in cache. X
+## and s are defined so that they can be accessed outside of
+## the function.
+## makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
@@ -17,8 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
              getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
+# The solve function assumes a square matrix is given
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
